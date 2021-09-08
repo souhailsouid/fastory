6113fc8ce4bffce26531e3ccb8b6eeffbaa77e05
souhail <mr.souid@live.fr>
2021-09-08 15:26:17 +0200

    delete comment in private route js

--- src/middleware/private-route.js
+++ src/middleware/private-route.js
@@ -3,8 +3,6 @@ import React from 'react'
 
 import { Route, Redirect } from 'react-router-dom'
 
-// import { Router } from 'react-router-dom/cjs/react-router-dom.min'
-
 function PrivateRoute ({ component, isAuthenticated, ...rest }) {
   return (
     <Route
